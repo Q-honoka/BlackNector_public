d80903d6666813c79cfddf86905778fc114aa2d1
Honoka <ncr2419gp0006@edu.nca.ac.jp>
2025-12-17 10:29:53 +0900

    Revert "ブリキの人形にエネミーインターフェースを実装しました"
    This reverts commit 89164820f0aa080a77e2cbf0cd5eb7506651f5a8.

--- Assets/Kamibeppu/Scripts/TinDoll/TinDollController.cs
+++ Assets/Kamibeppu/Scripts/TinDoll/TinDollController.cs
@@ -1,11 +1,10 @@
 using UnityEngine;
 
-public class TinDollController : MonoBehaviour, ICharcters, IEnemy
+public class TinDollController : MonoBehaviour, ICharcters
 {
 
     public enum TinDollState
     {
-
         MOVE,   // 移動
         TURN,   // Uターン
         END,    // 終了
@@ -15,8 +14,6 @@ public class TinDollController : MonoBehaviour, ICharcters, IEnemy
     [SerializeField] TinDollState state;
     [SerializeField] Vector3[] patrolPos = new Vector3[2];      // 巡回ポイント
     [SerializeField] int direction = 1;
-
-    private bool foundChild = false;
 
     private void Start()
     {
@@ -77,23 +74,4 @@ public class TinDollController : MonoBehaviour, ICharcters, IEnemy
 
         tinDoll.myData.charctersInterface.SetMyState((int)TinDollState.MOVE);
     }
-
-    /*  以下、IEnemy の実装です。敵の基本動作を実装しています。
-        FoundPlayer()       子どもを見つけたかどうかを true / false で設定する関数
-        GetFoundPlayer()    子どもを見つけたかどうかを true / false で返す関数
-        CatchByNector()     暗闇でネスターに見つかった時の処理をする関数（現状未定）
-     */
-    /// <summary>
-    /// Playerを見つけた
-    /// </summary>
-    void IEnemy.FoundPlayer() { foundChild = true; }
-    /// <summary>
-    /// playerの捜索状態を共有
-    /// </summary>
-    /// <returns></returns>
-    bool IEnemy.GetFoundPlayer() { return foundChild; }
-    /// <summary>
-    /// ネスターに捕まった
-    /// </summary>
-    void IEnemy.CatchByNector() { }
 }
