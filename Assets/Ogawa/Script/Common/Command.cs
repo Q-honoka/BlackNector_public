60dd2d106676a9c6d436acbaf846dcb7aed7f3e1
602-11\cre <k>
2025-12-19 11:34:58 +0900

    プレイヤーが移動したときにテキストを出す

--- Assets/Ogawa/Script/Common/Command.cs
+++ Assets/Ogawa/Script/Common/Command.cs
@@ -2,7 +2,6 @@ using Cysharp.Threading.Tasks;
 using RaruLib;
 using System;
 using UnityEngine;
-using static UnityEngine.Rendering.DebugUI;
 
 public class Command : MonoBehaviour
 {
--- Assets/Ogawa/takeOver_Maruyama/Sprict/Player/ChildController.cs
+++ Assets/Ogawa/takeOver_Maruyama/Sprict/Player/ChildController.cs
@@ -168,10 +168,15 @@ public class ChildController : MonoBehaviour, ICharcters
 
     public void SetCanMove() 
     {
-        Debug.Log($"あああ：{state}");
         if (state == CharctersState.MOVE) { child.myData.charctersInterface.SetMyState((int)CharctersState.IDLE); return; }
         else if (state == CharctersState.IDLE) { child.myData.charctersInterface.SetMyState((int)CharctersState.MOVE); return; }
             
+    }
+
+    public CharctersState SetCanMoveAndStateReturn()
+    {
+        SetCanMove(); 
+        return state;
     }
 
     public void ResetPos()
