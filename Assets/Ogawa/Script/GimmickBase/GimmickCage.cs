fd4cedef89970062e61bcd943aa309280ff64e24
503-12\cre <s>
2025-12-19 15:10:46 +0900

    ｘｘ

--- Assets/Ogawa/Script/GimmickBase/GimmickCage.cs
+++ Assets/Ogawa/Script/GimmickBase/GimmickCage.cs
@@ -2,12 +2,16 @@ using UnityEngine;
 
 public class GimmickCage : GimmickBase
 {
+    [SerializeField] private Rigidbody rigid;
+    [SerializeField] private Collider coll;
     /// <summary>
     /// ステートがTrueに変化したとき
     /// </summary>
     protected override void OnStateTrue()
     {
-
+        rigid.useGravity = true;
+        rigid.isKinematic = false;
+        coll.isTrigger = true;
     }
 
     /// <summary>
