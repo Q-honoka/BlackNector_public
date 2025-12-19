722b36188aab713a47fb1748de83111032542972
ncr2419gp0006 <ncr2419gp0006@edu.nca.ac.jp>
2025-12-19 14:29:11 +0900

    扉の開閉仮完成。

--- Assets/Ogawa/Script/GimmickBase/GimmickShutter.cs
+++ Assets/Ogawa/Script/GimmickBase/GimmickShutter.cs
@@ -2,12 +2,31 @@ using UnityEngine;
 
 public class GimmickShutter : GimmickBase
 {
+    [SerializeField] bool isOpen = false;   // 開いているか
+    [SerializeField, Header("デバッグ用扉")] GameObject door;
+
+    private void Update()
+    {
+        if(isOpen)
+        {
+            // 開くアニメーション
+            Debug.Log($"{name}: 開く");
+            door.SetActive(false);
+        }
+        else
+        {
+            // 閉まるアニメーション
+            Debug.Log($"{name}: 閉まる");
+            door.SetActive(true);
+        }
+    }
+
     /// <summary>
     /// ステートがTrueに変化したとき
     /// </summary>
     protected override void OnStateTrue()
     {
-
+        isOpen = !isOpen;
     }
 
     /// <summary>
@@ -15,6 +34,6 @@ public class GimmickShutter : GimmickBase
     /// </summary>
     protected override void OnStateFalse()
     {
-
+        isOpen = !isOpen;
     }
 }
