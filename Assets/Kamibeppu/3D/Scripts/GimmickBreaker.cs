344e00a7b607bea228aec9bfb293dec200d8e655
ncr2419gp0006 <ncr2419gp0006@edu.nca.ac.jp>
2026-01-16 14:57:13 +0900

    最終ステージのライトの点滅とブレーカーギミック実装

---
+++ Assets/Kamibeppu/3D/Scripts/GimmickBreaker.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GimmickBreaker : GimmickBase
+{
+    [SerializeField]
+    GimmickCollision collision;
+    [SerializeField]
+    GameObject[] lights;    // 消したいライトすべて
+    bool myState = true;
+    private Animator anim;
+
+    int pickedCount = 0;
+
+    private void Start()
+    {
+        isState = false;
+        anim = GetComponent<Animator>();
+        StartCoroutine(LightOffAnimation());
+    }
+
+    private void Update()
+    {
+        if (collision == null) { return; }
+
+        if (collision.awakeGimmick)
+        {
+            myState ^= true;
+            isState = myState;
+            collision.awakeGimmick = false;
+        }
+    }
+
+    // ライトを消すコルーチン
+    IEnumerator LightOffAnimation()
+    {
+        foreach (var light in lights)
+        {
+            yield return new WaitForSeconds(3.0f);
+            light.transform.GetComponentInChildren<Animator>().SetTrigger("Off");
+        }
+    }
+
+    // タイトルに戻る
+    IEnumerator ToTitle()
+    {
+        yield return new WaitForSeconds(15.0f);
+        SceneController.instance.SceneChange(0);
+    }
+
+    /// <summary>
+    /// ステートがTrueに変化したとき
+    /// </summary>
+    protected override void OnStateTrue()
+    {
+        pickedCount++;
+        anim.SetTrigger("Picked");
+        anim.SetInteger("PickedCount", pickedCount);
+
+        // 3回つついたらライトをつける
+        if (pickedCount == 3)
+        {
+            foreach (var light in lights)
+            {
+                light.transform.GetComponentInChildren<Animator>().SetTrigger("On");
+            }
+            pickedCount = 0;
+            StartCoroutine(ToTitle());
+        }
+    }
+
+    /// <summary>
+    /// ステートがFalseに変化したとき
+    /// </summary>
+    protected override void OnStateFalse()
+    {
+        
+    }
+
+}
