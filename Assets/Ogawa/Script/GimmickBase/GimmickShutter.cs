64a609796bc91480752e1cf6b32cb531e170c76f
ncr2419gp0006 <ncr2419gp0006@edu.nca.ac.jp>
2026-01-16 11:44:54 +0900

    扉ギミック実装

--- Assets/Ogawa/Script/GimmickBase/GimmickShutter.cs
+++ Assets/Ogawa/Script/GimmickBase/GimmickShutter.cs
@@ -2,12 +2,28 @@ using UnityEngine;
 
 public class GimmickShutter : GimmickBase
 {
+    [SerializeField] bool isOpen = false;
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.SetBool("IsOpen", isOpen);
+
+        // 始めから開いている場合はアニメーションをスキップ
+        if(isOpen)
+        {
+            anim.Play("DoorOpen", 0, 1.0f);
+        }
+    }
+
     /// <summary>
     /// ステートがTrueに変化したとき
     /// </summary>
     protected override void OnStateTrue()
     {
-
+        isOpen = !isOpen;
+        anim.SetBool("IsOpen", isOpen);
     }
 
     /// <summary>
@@ -15,6 +31,7 @@ public class GimmickShutter : GimmickBase
     /// </summary>
     protected override void OnStateFalse()
     {
-
+        isOpen = !isOpen;
+        anim.SetBool("IsOpen", isOpen);
     }
 }
