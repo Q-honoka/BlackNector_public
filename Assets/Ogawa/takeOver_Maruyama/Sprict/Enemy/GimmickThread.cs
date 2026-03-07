4932f279b8b950e207b6a37a3b30178625e22aed
ncr2419gp0006 <ncr2419gp0006@edu.nca.ac.jp>
2026-03-07 11:57:58 +0900

    つつくことが出来る場所にエフェクトを追加

--- Assets/Ogawa/takeOver_Maruyama/Sprict/Enemy/GimmickThread.cs
+++ Assets/Ogawa/takeOver_Maruyama/Sprict/Enemy/GimmickThread.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GimmickThread : GimmickBase
 {
-
+    [SerializeField] GameObject actionParticle;     // エフェクトオブジェクト
     bool connectMarionntte = true;
     GimmickCollision collision;
 
@@ -41,6 +41,10 @@ public class GimmickThread : GimmickBase
     protected override void OnStateFalse()
     {
         connectMarionntte = false;
+        if(actionParticle != null)
+        {
+            actionParticle.SetActive(false);
+        }
     }
 
     public bool GetConnctMarionnet() { return connectMarionntte; }
